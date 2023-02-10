4e6c1e27bb62f720bbe2ca014d0459d8104174ea
Gerson Garayar <garayargerson20@gmail.com>
2023-02-10 08:40:45 -0500

    fix: cambios nuevos

--- src/components/login-signup/Form.tsx
+++ src/components/login-signup/Form.tsx
@@ -1,4 +1,3 @@
-import React, { useState } from "react";
 import React,{useState} from "react";
 /* import { Navigate } from "react-router-dom"; */
 
@@ -11,10 +10,6 @@ data?:{
   message:string,
   status:string
 }
-interface Props {
-	title: string;
-	useMutate: () => {} | undefined;
-	loading: boolean;
 }
 
 interface StateForm{
@@ -23,21 +18,8 @@ interface StateForm{
   password:string,
   email:string
   }
-interface StateForm {
-	datos: {
-		name: string;
-		password: string;
-		email: string;
-	};
 }
 
-const Form = ({ title, useMutate, loading }: Props) => {
-	const [datos, setDatos] = useState<StateForm["datos"]>({
-		name: "",
-		password: "",
-		email: "",
-	});
-	const [showPassword, setShowPassword] = useState(false);
 
 
 const Form=({title,useMutate,loading}:Props)=>{
@@ -73,28 +55,13 @@ const Form=({title,useMutate,loading}:Props)=>{
 
 
 
-	const handleChange = (evt: React.ChangeEvent<HTMLInputElement>) => {
-		setDatos({
-			...datos,
-			[evt.target.name]: evt.target.value,
-		});
-	};
 
-	const handleSubmit = (evt: React.FormEvent<HTMLFormElement>) => {
-		evt.preventDefault();
-		console.log(datos);
-		useMutate(datos);
-	};
-	const handleClickShowPassword = () => {
-		setShowPassword(!showPassword);
-	};
+  /*variable para la interfaz */
+  const lenghtPassword= datos.password.length
 
-	/*variable para la interfaz */
-	const lenghtPassword = datos.password.length;
-
-	const menorA5 = lenghtPassword > 0 && lenghtPassword < 5;
-	const menorA10 = lenghtPassword >= 5 && lenghtPassword < 10;
-	const mayorA10 = lenghtPassword >= 10;
+  const menorA5 = lenghtPassword>0 && lenghtPassword<5
+  const menorA10 = lenghtPassword>=5&& lenghtPassword<10
+  const mayorA10 = lenghtPassword>=10
 
 
 
@@ -107,39 +74,18 @@ return(
         {title.toLocaleLowerCase() == "registrate" && <div>
           <label htmlFor="email" className="block mb-1 text-gray-600 font-semibold">nombre de Usuario</label>
 
-								<input
-									onChange={handleChange}
-									type="text"
-									className="bg-indigo-50 px-4 py-2 outline-none rounded-md w-full"
-									name='name'
-									required
-								/>
-							</div>
-						)}
-						<div>
-							<label
-								htmlFor="email"
-								className="block mb-1 text-gray-600 font-semibold"
-							>
-								Email
-							</label>
+          <input onChange={handleChange} type="text" className="bg-indigo-50 px-4 py-2 outline-none rounded-md w-full" name='name' required />
 
-							<input
-								onChange={handleChange}
-								type="email"
-								className="bg-indigo-50 px-4 py-2 outline-none rounded-md w-full"
-								name='email'
-								required
-							/>
-						</div>
+        </div>}
+        <div>
+          <label htmlFor="email" className="block mb-1 text-gray-600 font-semibold">Email</label>
 
-						<div className="relative">
-							<label
-								htmlFor="email"
-								className="block mb-1 text-gray-600 font-semibold"
-							>
-								Contraseña
-							</label>
+          <input onChange={handleChange} type="email" className="bg-indigo-50 px-4 py-2 outline-none rounded-md w-full" name='email' required />
+
+        </div>
+
+        <div className="relative">
+          <label htmlFor="email" className="block mb-1 text-gray-600 font-semibold">Contraseña</label>
 
           <input onChange={handleChange} type={showPassword ?"text" :"password"}  className="bg-indigo-50 px-4 py-2 outline-none rounded-md w-full" name='password' required minLength={8} />
           <div className="absolute   bottom-2 right-2" >
@@ -197,58 +143,5 @@ return(
   )
 
 }
-							<input
-								onChange={handleChange}
-								type={showPassword ? "text" : "password"}
-								className="bg-indigo-50 px-4 py-2 outline-none rounded-md w-full"
-								name='password'
-								required
-								minLength={8}
-							/>
-							<div className="absolute   bottom-2 right-2">
-								<svg
-									/* onClick={handleClickShowPassword} */
-									xmlns="http://www.w3.org/2000/svg"
-									width="24"
-									height="24"
-									viewBox="0 0 24 24"
-									fill="none"
-									stroke="currentColor"
-									stroke-width="2"
-									stroke-linecap="round"
-									stroke-linejoin="round"
-								>
-									<path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
-									<circle cx="12" cy="12" r="3"></circle>
-									{!showPassword && <line x1="1" y1="1" x2="23" y2="23"></line>}
-								</svg>
-							</div>
-						</div>
-						<div className={`grid grid-flow-col grid-cols-3`}>
-							<span
-								className={`h-1  
-          ${menorA5 && "bg-orange-500 col-span-1"}
-          ${menorA10 && "col-span-2 bg-yellow-500"} 
-          ${mayorA10 && "col-span-3 bg-green-500"}`}
-							></span>
-						</div>
-						<div>
-							{menorA5 && "contraseña debil"}
-							{menorA10 && "contraseña Medio"}
-							{mayorA10 && "contraseña Fuerte"}
-						</div>
-					</div>
-					<button className="h-12 mt-4 w-full bg-gradient-to-tr from-blue-600 to-indigo-600 text-indigo-100 py-2 rounded-md text-lg tracking-wide grid place-content-center">
-						{loading ? (
-							<span className="w-8 h-8 border-4 border-white border-b-red-600 rounded-full inline-block box-border animate-spin"></span>
-						) : (
-							title
-						)}
-					</button>
-				</div>
-			</form>
-		</div>
-	);
-};
 
-export default Form;
+export  default Form;
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,7 +1,7 @@
 import Signup from "../pages/signup/index";
 import Home from "../pages/home";
-import React from "react";
 import { Route, Routes } from "react-router-dom";
+import Login from '../pages/login'
 
 /* components */
 
@@ -11,6 +11,7 @@ export default function Router() {
 			<Routes>
 				<Route path="/" element={<Home />}></Route>
 				<Route path="/signup" element={<Signup />}></Route>
+        <Route path="/login" element={<Login/>}></Route>
 			</Routes>
 		</div>
 	);
