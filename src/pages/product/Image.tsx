aa79367b809431391e3db8e48f7a03283d085b28
Gerson Garayar <garayargerson20@gmail.com>
2023-02-18 11:31:37 -0500

    feat: add zoom

---
+++ src/pages/product/Image.tsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+
+export default function Image({ src }: { src: string }) {
+  const [position, setPosition] = useState({
+    x: 0,
+    y: 0,
+  });
+
+  const handleMove = (e: React.MouseEvent<HTMLImageElement, MouseEvent>) => {
+    const w = e.target.width;
+    const h = e.target.height;
+
+    const hw = w / 2;
+    const hh = h / 2;
+
+    const x = e.nativeEvent.offsetX;
+    const y = e.nativeEvent.offsetY;
+
+    setPosition({
+      x: ((x - hw) / hw) * 200,
+      y: ((y - hh) / hh) * 200,
+    });
+  };
+
+  return (
+    <div className="flex ">
+      <img onMouseMove={handleMove} className="border " src={src} />
+      <div className=" border relative overflow-hidden w-32 h-32">
+        <img
+          className="absolute"
+          style={{
+            scale: "300%",
+            right: `${position.x}px`,
+            bottom: `${position.y}px`,
+          }}
+          src={src}
+        />
+      </div>
+    </div>
+  );
+}
--- src/pages/product/index.tsx
+++ src/pages/product/index.tsx
@@ -1,8 +1,7 @@
-import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { useProduct } from "../../hooks/useProduct";
-import { Product } from "../../types/types";
 import { useStore } from "../../stores/productStore";
+import Image from "./Image";
 
 export default function index() {
   const id = useParams().id || "";
@@ -18,15 +17,18 @@ export default function index() {
 
   if (isLoading) return <h2>cargando...</h2>;
   return (
-    <div>
+    <div className="max-w-7xl m-auto">
       <section className="grid md:grid-cols-2">
-        <article>
-          <img src={data?.image_url} alt="" />
+        <article className="border flex justify-center p-8">
+          <Image src={data?.image_url} />
         </article>
-        <article>
-          <h2>{data?.title}</h2>
-          <p>{data?.price}</p>
-          <button onClick={handleClick} className="bg-sky-500 p-4">
+        <article className="p-8 flex flex-col gap-4">
+          <h2 className="text-2xl">{data?.title}</h2>
+          <p className="text-xl text-red-500 font-bold">S/ {data?.price}</p>
+          <button
+            onClick={handleClick}
+            className="bg-sky-500 text-white px-4 py-2 rounded shadow hover:bg-sky-600 w-60"
+          >
             añadir al carrito
           </button>
         </article>
