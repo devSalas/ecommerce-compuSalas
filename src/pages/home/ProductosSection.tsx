18a716839477074b1ca1bd8ce45920fc0c5ae7bc
Gerson Garayar <garayargerson20@gmail.com>
2023-02-22 22:54:57 -0500

    feat: loader

--- src/pages/home/ProductosSection.tsx
+++ src/pages/home/ProductosSection.tsx
@@ -2,12 +2,16 @@ import React from "react";
 import Card from "../../components/home/Card";
 import { useProducts } from "../../hooks/useProduct";
 import { Product } from "../../types/types";
+import LoadingProduct from "./LoadingProduct";
 
 export default function ProductosSection() {
   const { data, isLoading, error } = useProducts();
   console.log(data);
+
+  if (isLoading) return <LoadingProduct />;
+
   return (
-    <section className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+    <section className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 py-8">
       <h2></h2>
       {data?.products?.map(
         ({ _id, title, price, category, image_url, description }: Product) => (
