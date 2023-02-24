6dae0145dc4cb6f08a9ef78f393b0fc6253a2432
esteban salas <esalassulca@gmail.com>
2023-02-24 18:17:02 -0500

    feat: animation cantidad de productos

--- src/components/global/Header/BtnCarrito.tsx
+++ src/components/global/Header/BtnCarrito.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+
 import Contador from "./Contador";
 import Carrito from "../../icons/Carrito";
 import { useStore } from "../../../stores/productStore";
@@ -6,19 +6,24 @@ import { Link } from "react-router-dom";
 
 export default function BtnCarrito() {
   const store = useStore();
+  let cantidadProductos = 0;
+  store.Products.forEach(product => {
+    if(!product.cantidad) return
+      cantidadProductos += product.cantidad
+  })
 
   return (
     <button className="flex items-center gap-6">
       <div className="relative">
         <Link to={"/checkout"}>
           <Carrito />
-          <Contador numero={store.Products.length} />
+          <Contador numero={cantidadProductos} />
         </Link>
       </div>
       <div className="text-left md:block hidden">
         <Link to={"/checkout"}>
           <p className="text-slate-500 text-sm">Total</p>
-          <p className="font-semibold">$0.00</p>
+          <p className="font-semibold">{store.totalPrice}</p>
         </Link>
       </div>
     </button>
--- src/components/global/Header/Contador.tsx
+++ src/components/global/Header/Contador.tsx
@@ -1,9 +1,23 @@
-import React from "react";
+import { useRef } from 'react';
+import style from './Contador.module.css'
 
 export default function Contador({ numero }: { numero: number }) {
+  const isAdded = useRef<HTMLDivElement>(null);
+  if(numero){
+    if(isAdded.current){
+      isAdded.current.classList.add(style.flip)
+      setTimeout(() => {
+        if(isAdded.current){
+          isAdded.current.classList.remove(style.flip)
+        }
+        
+      }, 500);
+      
+    }
+  }
   return (
-    <div className="bg-red-500 absolute -top-2 -right-2 text-white w-5 h-5 flex items-center justify-center rounded-full text-sm">
+    <div ref={isAdded} className="bg-red-500 absolute -top-2 -right-2 text-white w-5 h-5 flex items-center justify-center rounded-full text-sm">
       {numero}
     </div>
-  );
+  )
 }
