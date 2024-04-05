aa218623c8d00938222647d33fefe1a810c2d0d2
estebans salas sulca <esalassulca@gmail.com>
2024-04-05 16:41:55 -0500

    agregar nueva dominio del backend

--- src/services/FetchProduct.ts
+++ src/services/FetchProduct.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const url = "https://ecomerce-backend-wo0w.onrender.com/api/product";
+/* const url = "https://ecomerce-backend-wo0w.onrender.com/api/product"; */
+const url = "http://compusalas.sunetya.site/api/product";
 
 export const getProducts = () => {
 	return axios.get(url).then((res) => res.data);
