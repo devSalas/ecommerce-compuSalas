8818a8dc7fc2a36dac55f4c7b4b3f129af178cca
estebans salas sulca <esalassulca@gmail.com>
2024-04-05 17:23:53 -0500

    agregar el https a la sintaxis del dominio backend

--- src/services/FetchProduct.ts
+++ src/services/FetchProduct.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 /* const url = "https://ecomerce-backend-wo0w.onrender.com/api/product"; */
-const url = "http://compusalas.sunetya.site/api/product";
+const url = "https://compusalas.sunetya.site/api/product";
 
 export const getProducts = () => {
 	return axios.get(url).then((res) => res.data);
